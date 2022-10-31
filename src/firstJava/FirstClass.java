5073ccd5ffa727afad2498dc287e362cd4c83335
Gulbakhor Ollaberganova <gulbakhorollaberganova@gmail.com>
2022-10-30 20:11:57 -0700

    first commit

---
+++ src/firstJava/FirstClass.java
@@ -0,0 +1,12 @@
+package firstJava;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	int a = 9;
+	int b = 8;
+	System.out.println(a + b);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author gulba
+ *
+ */
+module Variables {
+}
